b467557ad3e7d72c120fa6ba8c020392eb1497f6
Einee <yannokaiserfrom@gmail.com>
2023-06-18 19:39:54 -0300

    recuperacion de peticiones guardadas finalizada

--- main.js
+++ main.js
@@ -25,10 +25,12 @@ const send_http_axios=(data,option)=>{
         })
         .catch(error => {
           // Manejo del error
-          console.error(error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Escribiste un dato de peticion erroneo revisa por favor',
+            text: 'Revisa que los datos sean correctos!'
+          })
         });
-
-
 }
 
 function sendRequest() {
@@ -48,52 +50,112 @@ function sendRequest() {
   }
   
   let saveHttp=()=>{
-
-    let data = dataH.value;
-    let method = methodH.value;
-    let url = urlH.value;
-    let path = pathH.value;
-    let token = tokenH.value;
-  
-    var option = {
-      method: method,
-      url: url,
-      path: path,
-      token: token,
-      data:data
-    };
-     let saved=JSON.parse(localStorage.getItem("peticiones"))
+      
+      let data = dataH.value;
+      let method = methodH.value;
+      let url = urlH.value;
+      let path = pathH.value;
+      let token = tokenH.value;
     
-     if(saved){
-      if (saved.length>=5) { 
-        Swal.fire({
-          icon: 'error',
-          title: 'solo se puede guardar 5 peticiones',
-          text: 'Intenta eliminar una peticion!'
-        })
-        return console.log("solo se puede guardar 5 peticiones");
-       }
-      let guardar= localStorage.setItem("peticiones",JSON.stringify([...saved,option]))
-      deleteQuery()
-      savedQuery()
-    }else{
-      let guardar= localStorage.setItem("peticiones",JSON.stringify([option]))
-      savedQuery()
-    }
+      var option = {
+        method: method,
+        url: url,
+        path: path,
+        token: token,
+        data:data
+      };
+       let saved=JSON.parse(localStorage.getItem("peticiones"))
+      
+       if(saved){
+        if (saved.length>=5) { 
+          Swal.fire({
+            icon: 'error',
+            title: 'solo se puede guardar 5 peticiones',
+            text: 'Intenta eliminar una peticion!'
+          })
+          return console.log("solo se puede guardar 5 peticiones");
+         }
+        let guardar= localStorage.setItem("peticiones",JSON.stringify([...saved,option]))
+        deleteQuery()
+        savedQuery()
+      }else{
+        let guardar= localStorage.setItem("peticiones",JSON.stringify([option]))
+        savedQuery()
+      }
+
   }
+
   let deleteQuery=()=>{
     peticiones.innerHTML=""
   }
 
-  let savedQuery=()=>{
+  const asigBoton=()=>{
+    let boton1=document.getElementById(`dato-1`)
+    let boton2=document.getElementById(`dato-2`)
+    let boton3=document.getElementById(`dato-3`)
+    let boton4=document.getElementById(`dato-4`)
+    let boton5=document.getElementById(`dato-5`)
+
+    boton1.addEventListener("click",(e)=>{ 
+      let pars=JSON.parse(e.target.textContent)
+       dataH.value = pars.data
+       methodH.value = pars.method
+       urlH.value = pars.url
+       pathH.value = pars.path
+       tokenH.value = pars.token
+    })
+
+    boton2.addEventListener("click",(e)=>{
+      let pars=JSON.parse(e.target.textContent)
+
+       dataH.value = pars.data
+       methodH.value = pars.method
+       urlH.value = pars.url
+       pathH.value = pars.path
+       tokenH.value = pars.token
+    })
+    boton3.addEventListener("click",(e)=>{
+      let pars=JSON.parse(e.target.textContent)
+
+       dataH.value = pars.data
+       methodH.value = pars.method
+       urlH.value = pars.url
+       pathH.value = pars.path
+       tokenH.value = pars.token
+    })
+    boton4.addEventListener("click",(e)=>{
+      let pars=JSON.parse(e.target.textContent)
+
+       dataH.value = pars.data
+       methodH.value = pars.method
+       urlH.value = pars.url
+       pathH.value = pars.path
+       tokenH.value = pars.token
+    })
+    boton5.addEventListener("click",(e)=>{
+      let pars=JSON.parse(e.target.textContent)
+
+       dataH.value = pars.data
+       methodH.value = pars.method
+       urlH.value = pars.url
+       pathH.value = pars.path
+       tokenH.value = pars.token
+    })
+  }
+
+  let savedQuery= ()=>{
 
     //datos de la peticion guardados
     let  Dguardados= JSON.parse(localStorage.getItem("peticiones"))
     if(Dguardados){
-    Dguardados.forEach(element => {
-      peticiones.innerHTML+=`<ul id="dato">${JSON.stringify(element)}<ul/>`
-      
+      let contador=0
+      Dguardados.forEach(element => {
+       contador++
+       return peticiones.innerHTML+=`<ul id="dato-${contador}">${JSON.stringify(element)}<ul/>`
     });
+      asigBoton()
+    
   }
   }
+  
   savedQuery()
